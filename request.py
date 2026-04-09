8975a075f60794f5819651d1d68bfc01bc557388
Paulinedjm <24160@ecam.be>
2026-04-09 15:12:13 +0200

    connexion?

--- request.py
+++ request.py
@@ -3,4 +3,24 @@ import json
 
 serverAddress = ("127.17.10.41", 3000)
 
-#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+def send_json(sock, data):
+    message = json.dumps(data).encode("utf-8")
+    sock.sendall(message)
+
+def receive_json(sock):
+    message = sock.recv(4).decode('utf-8')
+    return json.loads(message)
+
+data = {
+  "request": "subscribe",
+  "port": 3000,
+  "name": "fun_name_for_the_client",
+  "matricules": ["24343", "24160"] }
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client: 
+    client.connect(serverAddress)
+    send_json(client, data)
+    
+    response = receive_json(client)
+    
