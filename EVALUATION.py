8b92d2a11432901687cdc71be11279218750ca3b
cindy546 <24343@ecam.be>
2026-04-09 14:54:49 +0200

    Add changed

--- EVALUATION.py
+++ EVALUATION.py
@@ -6,7 +6,11 @@ serverAddress= ("127.17.10.41", 3000)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect(serverAddress)
     message= client.send()
-    with open("eval.json", "w")
+    with open("eval.json", "w", encoding='utf-8') as fichier
+    json.dump(fichier, indent=4, ensure_ascii=False)
+    #Cette fonction prend un objet Python et le convertit en une chaîne de caractères formatée en JSON, 
+    # sans l’écrire dans un fichier.
+    
     client.send()
     response = client.recv(4).decode()
     print(response)
