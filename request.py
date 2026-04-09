897d33a0a28899b82630c7393026d1a1ba74d6db
Paulinedjm <24160@ecam.be>
2026-04-09 16:00:05 +0200

    add

--- request.py
+++ request.py
@@ -1,20 +1,29 @@
 import socket
 import json
+import struct 
 
-serverAddress = ("127.17.10.41", 3000)
+serverAddress = ("172.17.10.41", 3000)
 
 def send_json(sock, data):
     message = json.dumps(data).encode("utf-8")
+    length = struct.pack('I', len(message)) 
+    sock.sendall(length)
     sock.sendall(message)
 
+
 def receive_json(sock):
-    message = sock.recv(4).decode('utf-8')
-    return json.loads(message)
+   length_data = sock.recv(4)
+   if not length_data:
+        return None
+   length = struct.unpack('I', length_data)[0]
+
+   message = sock.recv(length).decode('utf-8')
+   return json.loads(message)
 
 data = {
   "request": "subscribe",
-  "port": 3000,
-  "name": "fun_name_for_the_client",
+  "port": 8888,
+  "name": "Pauline",
   "matricules": ["24343", "24160"] }
 
 
@@ -23,4 +32,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     send_json(client, data)
     
     response = receive_json(client)
+
+
     
