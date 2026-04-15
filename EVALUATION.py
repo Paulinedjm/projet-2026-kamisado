86208980b6e32976d92e2afe805c361b2b7535e7
cindy546 <24343@ecam.be>
2026-04-15 13:57:31 +0200

    Addchanges

--- EVALUATION.py
+++ EVALUATION.py
@@ -2,15 +2,52 @@ import socket
 import json
 
 serverAddress= ("172.20.10.3", 3000)
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect((serverAddress))
-    message= client.send()
-    with open("eval.json", "w", encoding='utf-8') as 
-    json.dump(fichier, indent=4, ensure_ascii=False)
-    #Cette fonction prend un objet Python et le convertit en une chaîne de caractères formatée en JSON, 
-    # sans l’écrire dans un fichier.
     
-    client.send()
-    response = client.recv(4).decode()
-    print(response)
+
+def recevoir(client):
+    reçu={
+      "response": "ok"
+    }
+    message_reçu_erreur= 
+    {
+      "response": "error",
+      "error": "error message"
+    }
+    
+    with open("eval.json", "w") as message_reçu : 
+        message_reçu= json.loads(message_reçu)
+        message= client.recv(4).decode()
+    print(message)
+   
+    
+
+def envoyé(client):
+    envoyé= {
+      "request": "subscribe",
+      "port": 8888,
+      "name": "fun_name_for_the_client",
+      "matricules": ["12345", "67890"]
+    }
+    
+    message_envoyé= json.dumps(envoyé)
+
+    client.send(message_envoyé.encode())
+
+
+
+#requête serveur 
+
+request_envoyé=
+{
+  "request": "ping"
+}
+
+request_reçu= 
+{
+  "response": "pong"
+}
+
+
+
