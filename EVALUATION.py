ba6dcc492538f61922a2f113bee49637508964af
cindy546 <24343@ecam.be>
2026-04-15 13:29:35 +0200

    Ip changed

--- EVALUATION.py
+++ EVALUATION.py
@@ -1,12 +1,12 @@
 import socket 
 import json
 
-serverAddress= ("172.17.10.41", 3000)
+serverAddress= ("172.20.10.3", 3000)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-    client.connect(serverAddress)
+    client.connect((serverAddress))
     message= client.send()
-    with open("eval.json", "w", encoding='utf-8') as fichier
+    with open("eval.json", "w", encoding='utf-8') as 
     json.dump(fichier, indent=4, ensure_ascii=False)
     #Cette fonction prend un objet Python et le convertit en une chaîne de caractères formatée en JSON, 
     # sans l’écrire dans un fichier.
