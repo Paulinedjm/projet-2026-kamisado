71bb1afb77fa401e589a4d56375503a6a0255d6f
Paulinedjm <24160@ecam.be>
2026-04-22 22:22:02 +0200

    rajout de coup random + les coups autorisé

--- EVALUATION.py
+++ EVALUATION.py
@@ -105,6 +105,55 @@ def envoyer_coup(client, move):
     client.send(header + message_json)
 
 
+def find_tower_position(board, color_to_find, player_id):
+    """
+    board: la grille 8x8
+    color_to_find: la couleur imposée (ex: "RED")
+    player_id: ton numéro de joueur (ex: 0 ou 1)
+    """
+    for r in range(8): # Parcourt les lignes de 0 à 7
+        for c in range(8): # Parcourt les colonnes de 0 à 7
+            case = board[r][c]
+            
+            # On vérifie si la case n'est pas vide
+            if case is not None:
+                # On vérifie si c'est notre tour (player_id) 
+                # et si c'est la bonne couleur
+                if case['player'] == player_id and case['color'] == color_to_find:
+                    return r, c # On a trouvé ! On renvoie la position
+                    
+    return None # Si on n'a rien trouvé (ne devrait pas arriver)
+
+def get_legal_moves(state, color_to_play, player_id):
+    """
+    state: le plateau (grille 8x8)
+    color_to_play: la couleur de la tour que je DOIS bouger
+    player_id: 0 pour le joueur du bas (monte), 1 pour le joueur du haut (descend)
+    """
+    moves = [] #on cree une liste de coordonnées de coups possible
+    
+    # 1. Trouver les coordonnées (r, c) de la tour de la bonne couleur
+    pos_r, pos_c = find_tower_position(state, color_to_play, player_id)
+    
+    # 2. Définir les directions "avant" selon le joueur
+    direction = -1 if player_id == 0 else 1 #determine si on doit monter ou descendre dans le board en fonction du joueur qui joue, Si player_id == 0, on monte (ligne -1), si 1 on descend (ligne +1)
+    
+    # Les 3 colonnes à tester : gauche (-1), tout droit (0), droite (+1), explore les 3 directions 
+    for dc in [-1, 0, 1]:
+        r, c = pos_r + direction, pos_c + dc
+        
+        # Tant qu'on est sur le plateau et que la case est vide
+        while 0 <= r < 8 and 0 <= c < 8 and state[r][c] == None:
+            moves.append((r, c)) # On ajoute la coordonnée comme coup possible
+            
+            # On continue d'avancer dans cette direction (comme une tour d'échecs)
+            r += direction
+            c += dc
+            
+    return moves
+
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect((serverAddress))  
     taille_envoyé, taille_attendu = envoyé(client) #recupérer la taille du messages envoyé
@@ -112,12 +161,35 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     recevoir(client, taille_envoyé, taille_attendu)
 
     attendre_ping()
+
+
     while True:
         message = state(client)
         if message["request"] == "play":
-            etat = message["state"]
-            coup = random
-            envoyer_coup(client, coup)
+            # On récupère les infos du message serveur
+            plateau = message["state"]["board"]
+            couleur_voulue = message["state"]["next_color"]
+            mon_id = message["state"]["current_player"]
+
+            position_depart = find_tower_position(plateau, couleur_voulue, mon_id)   # On trouve d'abord où est notre tour (le départ)
+            r_dep, c_dep = position_depart     # On récupère les coordonnées de départ
+
+
+
+            # On génère les coups
+            coup = get_legal_moves(plateau, couleur_voulue, mon_id)
+    
+            if not coup:
+                reponse = {"response": "giveup"}
+            else:
+                r_arr, c_arr = random.choice(coup)    # Pour l'instant on choisit au hasard, plus tard ce sera le meilleur coup du Negamax
+
+                move = [
+                    [r_dep, c_dep], 
+                    [r_arr, c_arr]
+                ]  #notre move dans le bon format
+
+                reponse = {"response": "move", "move": move}
 
 
 
